61fd7c192116c48ff110c9cb550c43209e462b25
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2025-01-30 21:59:26 +0530

    add api documentation link

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(cookieParser());
 
+app.get("/api/docs", (req: Request, res: Response) => {
+    res.redirect("https://documenter.getpostman.com/view/39284494/2sAYX2N4Zu");
+});
+
 // Routes import
 import healthCheckRouter from "./routes/healthcheck.routes";
 import userRouter from "./routes/user.routes";
